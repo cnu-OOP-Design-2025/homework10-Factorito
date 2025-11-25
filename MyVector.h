8e8bdc39b1950e26b766b5ee99e0c8d113f58c6c
Factorito <redbulue0509@gmail.com>
2025-11-25 18:22:46 +0900

    과제제출

--- MyVector.h
+++ MyVector.h
@@ -2,10 +2,11 @@
 
 #include <initializer_list>
 #include <cstddef>
-
+#include <algorithm> 
+using namespace std;
 
 template<typename T>
-class MyVector {
+class MyVector{
     T* data;            // 동적 배열을 가리키는 포인터
     size_t cap;         // 현재 할당된 배열 크기
     size_t length;      // 실제 저장된 요소 개수    
@@ -20,26 +21,81 @@ public:
             data[i++] = val;
         }
     }
+     void resize() {
+        size_t newCap = (cap == 0) ? 1 : cap * 2;
+        T* newData = new T[newCap];
+        for (size_t i = 0; i < length; ++i) newData[i] = data[i];
+        delete[] data;
+        data = newData;
+        cap = newCap;
+    }
 
     MyVector(): data(nullptr), length(0), cap(0) {}
     ~MyVector() { delete[] data; }
 
-    void push_back(const T& val);   // Vector 마지막에 항목을 추가하는 함수 구현, 필요시 벡터 크기 증가
-    void pop_back();                // Vector의 마지막 항목을 제거하는 함수 구현
-    size_t size() const;            // Vecotr에 있는 항목 개수(length)를 리턴하는 함수 구현
-    T& operator[](int i) ;          // [] 연산자 구현
+    void push_back(const T& val){
+        if(cap > length){
+        data[length++] = val; 
+        return;
+    }else{
+        resize();
+        data[length++] = val;
+        return;
+    }
+};
+    void pop_back(){
+        if(length > 0){
+            --length;
+        }
+    }
 
-    bool operator==(const MyVector& other) const;
+    size_t size() const{
+        return length;
+    };         
+    T& operator[](int i){
+        return data[i];
+    }
 
-    bool operator!=(const MyVector& other) const;
+    bool operator==(const MyVector& other) const{
+        if (length != other.length) {
+            return false;
+        }
+        for (size_t i = 0; i < length; ++i) {
+            if (data[i] != other.data[i]) {
+                return false;
+            }
+        }
+        return true;
+    };
 
-    bool operator<(const MyVector& other) const;
+    bool operator!=(const MyVector& other) const{
+        return !(*this == other);
+    };
 
-    bool operator>(const MyVector& other) const;
+    bool operator<(const MyVector& other) const{
+        size_t min_len = std::min(length, other.length);
+        for (size_t i = 0; i < min_len; ++i) {
+            if (data[i] < other.data[i]) {
+                return true;
+            }
+            if (data[i] > other.data[i]) {
+                return false;
+            }
+        }
+        return length < other.length;
+    };
 
-    bool operator<=(const MyVector& other) const;
+    bool operator>(const MyVector& other) const{
+        return other < *this;
+    };
 
-    bool operator>=(const MyVector& other) const;
+    bool operator<=(const MyVector& other) const{
+        return !(*this > other);
+    };
+
+    bool operator>=(const MyVector& other) const{
+        return !(*this < other);
+    };
 
 
     // Vector에 대한 반복자
@@ -48,45 +104,82 @@ public:
     public:
         Iterator(T* p = nullptr): ptr(p) {}
 
-        T& operator*(); // 역참조 연산자 구현 
+        T& operator*(){return *ptr;};
 
-        Iterator& operator++();   //전위 증가 연산자 구현
-        Iterator& operator--();   //전위 감소 연산자 구현
-        Iterator operator+(int n) const ;   // 산술 연산자 구현
-        Iterator operator-(int n) const ;   // 산술 연산자 구현
+        Iterator& operator++(){ptr++;
+        return *this;};
+        Iterator& operator--(){
+            ptr--;
+            return *this;
+        };
+        Iterator operator+(int n) const{return Iterator(ptr + n);};   // 산술 연산자 구현
+        Iterator operator-(int n) const{return Iterator(ptr - n);};   // 산술 연산자 구현
 
-        bool operator==(const Iterator& other) const ;
-        bool operator!=(const Iterator& other) const ;
+        bool operator==(const Iterator& other)const {return ptr == other.ptr;};
+        bool operator!=(const Iterator& other) const{return ptr != other.ptr;};
 
-        int operator-(const Iterator& other) const ;
+        int operator-(const Iterator& other) const {return ptr - other.ptr;};
     };
 
     // 첫번째 항목에 대한 iterator 리턴 함수 구현
-    Iterator begin() ;
+    Iterator begin(){return Iterator(data);};
     // 마지막 항목에 대한 iterator 리턴 함수 구현 
-    Iterator end() ;
+    Iterator end(){return Iterator(data+length);};
 
 
     // insert: 지정 위치에 요소 삽입 함수 구현
-    Iterator insert(Iterator pos, const T& value);
+    Iterator insert(Iterator pos, const T& value){
+        size_t idx = pos-begin();
+        if (length == cap) resize();
+        for (size_t j = length; j > idx; --j) data[j] = data[j-1];
+        data[idx] = value;
+        ++length;
+        return Iterator(data + idx);
+        };
 
     // erase: 지정 위치 요소 제거 함수 구현
-    Iterator erase(Iterator pos);
+    Iterator erase(Iterator pos){
+        for(size_t i=0; i<length; i++){
+                if(&data[i] == &(*pos)){
+                    for(size_t j=i; j<length-1; j++){
+                        data[j] = data[j+1];
+                    }
+                    length--;
+                    break;
+                }
+            }
+            return pos;
+    };
 
     // clear: 모든 요소 제거 함수 구현 (size = 0 초기화)
-    void clear() ;
+    void clear(){
+        length = 0;
+    }
 
     // at: 범위 체크 후, i번째 항목 접근 함수 구현
-    T& at(size_t i); 
+    T& at(size_t i){
+         if(i<length){
+        return data[i];
+    }
+    throw out_of_range("index");
+    };
 
     // front/back
-    T& front() ;    // 첫번째 항목 리턴 함수 구현
-    T& back() ;     // 마지막 항목 리턴 함수 구현
+    T& front(){
+        return data[0];
+    };   // 첫번째 항목 리턴 함수 구현
+    T& back(){
+        return data[length -1];
+    } ;     // 마지막 항목 리턴 함수 구현
 
     // capacity
-    size_t capacity() const ;   // cap 값 리턴하는 함수 구현
+    size_t capacity() const{
+          return cap;
+    } ;   // cap 값 리턴하는 함수 구현
 
-    // empty
-    bool empty() const ;    // 현재 vector가 empty인지 확인하는 함수 구현
-
-};
+        // empty
+        bool empty() const{
+            return length == 0;
+        };    // 현재 vector가 empty인지 확인하는 함수 구현
+    
+    };
